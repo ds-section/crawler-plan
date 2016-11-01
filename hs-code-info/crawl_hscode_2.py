476eb348dcf2a96a2a6a62b045911bf9867638b9
Shih-Po <spurcheng@gmail.com>
2016-11-01 16:44:51 +0800

    add hscode_2, 4 crawler and results

---
+++ hs-code-info/crawl_hscode_2.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+"""
+get all hscode_2
+part 1. hscode_2, description, link
+"""
+
+# get all hs code list and link 
+r = requests.get('http://db.wtocenter.org.tw/tariffHScode.asp')
+r.encoding = 'big5'
+
+# parse 
+soup = BeautifulSoup(r.text, 'lxml')
+td_main_top = soup.find_all('td', class_='mainText', valign='top')
+
+print('hscode_2', 'text', 'url', sep=',')
+
+for e in td_main_top:
+
+    url = e.find('a').get('href')
+    hscode_2 = url[-2:]
+    # from same hierarchies with e to find next td
+    text = e.parent.find_all('td', class_='mainText')[1].find('font').text
+
+    print(hscode_2, text, url, sep=',')
+
+# for e in td_main:
+#     print(e)
+#     print('\n\n')
---
+++ hs-code-info/crawl_hscode_4.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+"""
+get all hscode_4 by urls in hscode2.csv
+"""
+def print_hscode_4_from_2(url_2):
+
+    r = requests.get(url_2)
+    r.encoding = 'big5'
+    soup = BeautifulSoup(r.text, 'lxml')
+    td_main_top = soup.find_all('td', class_='mainText', valign='top')
+
+    for e in td_main_top:
+
+        url_4 = e.find('a').get('href')
+        hscode_4 = url_4[-4:]
+        # from same hierarchies with e to find next td
+        text = e.parent.find_all('td', class_='mainText')[1].find('font').text
+        text = text.strip()
+
+        print(hscode_4, text, url_4, sep=',')
+
+url_list = []
+with open('hscode2.csv', 'r') as f:
+    
+    for line in f:
+
+        url = line.split(',')[2].replace('\n', '')
+        url_list.append('http://db.wtocenter.org.tw/' + url)
+
+    # filter headers out
+    url_list = url_list[1::]
+
+
+print('hscode_4', 'text', 'url', sep=',')
+
+for url in url_list:
+    print_hscode_4_from_2(url)
